100e041ef53bfe338080ce8824197ea005f82624
srackley <shelbytrackley@gmail.com>
2018-03-07 21:56:19 -0500

    eject

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './styles/index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
+render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
-  , document.getElementById('root')
+  , document.body
 );
 registerServiceWorker();
